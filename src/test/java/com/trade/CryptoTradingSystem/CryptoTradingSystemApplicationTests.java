84cbc87e448a7e9b4fbe354974db9b25ed76d178
Rakesh Palle <rpalle@binghamton.edu>
2025-01-27 17:41:58 -0600

    Initial commit

---
+++ src/test/java/com/trade/CryptoTradingSystem/CryptoTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.CryptoTradingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
